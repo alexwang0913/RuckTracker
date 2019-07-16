9c809413e6a9508ea75865e2fcc42dd5f558f83c
Guang <guangwen1221@gmail.com>
2019-07-16 11:22:55 -0400

    Initial Commit

---
+++ RuckTracker/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  RuckTracker
+//
+//  Created by Guangyuan Xu on 2019/7/16.
+//  Copyright © 2019 Guang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
